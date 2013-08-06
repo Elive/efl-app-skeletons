11676cb69b72a583c126335e350b63b00ed895e9
Jérémy Zurcher <jeremy@asynk.ch>
2013-08-06 14:51:45 +0200

    app: config.h included in *.c, add extern 'C'

--- app/src/lib/elm_skel.h
+++ app/src/lib/elm_skel.h
@@ -1,10 +1,17 @@
 #ifndef ELM_SKEL_H
 #define ELM_SKEL_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <Elementary.h>
-#include <config.h>
 
 EAPI const char*
 elm_skel_hello();
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif   /* ELM_SKEL_H */
