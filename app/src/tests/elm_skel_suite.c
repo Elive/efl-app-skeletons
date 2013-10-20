70bfb1be580ce8cbc0fdce78bc5352627b9e9d93
Jérémy Zurcher <jeremy@asynk.ch>
2013-10-20 23:19:45 +0000

    app: fix indent in test suite

--- app/src/tests/elm_skel_suite.c
+++ app/src/tests/elm_skel_suite.c
@@ -7,37 +7,37 @@
 
 START_TEST (test_elm_skel_lib)
 {
-    ck_assert(elm_skel_lib_init() == 1);
-    ck_assert(elm_skel_lib_init() == 2);
+   ck_assert(elm_skel_lib_init() == 1);
+   ck_assert(elm_skel_lib_init() == 2);
 
-    ck_assert(strcmp(elm_skel_lib_hello(), "Hello from the EFL world") == 0);
+   ck_assert(strcmp(elm_skel_lib_hello(), "Hello from the EFL world") == 0);
 
-    ck_assert(elm_skel_lib_shutdown() == 1);
-    ck_assert(elm_skel_lib_shutdown() == 0);
-    ck_assert(elm_skel_lib_shutdown() == 0);
+   ck_assert(elm_skel_lib_shutdown() == 1);
+   ck_assert(elm_skel_lib_shutdown() == 0);
+   ck_assert(elm_skel_lib_shutdown() == 0);
 }
 END_TEST
 
 Suite *
 elm_skel_suite (void)
 {
-    Suite *s = suite_create ("Elm_Skel");
+   Suite *s = suite_create ("Elm_Skel");
 
-    TCase *tc_core = tcase_create ("Core");
-    tcase_add_test (tc_core, test_elm_skel_lib);
-    suite_add_tcase (s, tc_core);
+   TCase *tc_core = tcase_create ("Core");
+   tcase_add_test (tc_core, test_elm_skel_lib);
+   suite_add_tcase (s, tc_core);
 
-    return s;
+   return s;
 }
 
 int
 main(int argc EINA_UNUSED, char *argv[] EINA_UNUSED)
 {
-    int number_failed;
-    Suite *s = elm_skel_suite();
-    SRunner *sr = srunner_create(s);
-    srunner_run_all(sr, CK_ENV);
-    number_failed = srunner_ntests_failed(sr);
-    srunner_free(sr);
-    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+   int number_failed;
+   Suite *s = elm_skel_suite();
+   SRunner *sr = srunner_create(s);
+   srunner_run_all(sr, CK_ENV);
+   number_failed = srunner_ntests_failed(sr);
+   srunner_free(sr);
+   return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
